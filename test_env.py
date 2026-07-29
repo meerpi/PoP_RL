e68bc6945ccad38ed5de0feda201ee8cc9af746f
meerpi <aritra.mahanty12@gmail.com>
2026-07-29 21:40:12 +0530

    refactor: prune obsolete experimental scripts and legacy proxies

--- test_env.py
+++
@@ -1,8 +0,0 @@
-import PoP_env.envs.PoP_env as pop
-import numpy as np
-env = pop.PoPEnv(headless=True)
-obs, info = env.reset()
-print("init frame:", env.obs_builder.kid.frame)
-for act in [0, 0, 0, 4, 4, 4, 0, 0]:
-    obs, r, term, trunc, info = env.step(act)
-    print(f"action: {act} -> elapsed: {env.last_tau}, frame: {env.obs_builder.kid.frame}")
